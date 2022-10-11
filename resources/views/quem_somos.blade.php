7943f2fc0c4c7a40dc89054f31b2e4415a5bd0ae
Luiz Gabriel <91032465+osgabrielluiz@users.noreply.github.com>
2022-10-11 05:12:14 -0300

    quem somos

--- resources/views/quem_somos.blade.php
+++ resources/views/quem_somos.blade.php
@@ -1,20 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/quemSomos.css">
     <link rel="Icon" href="/img/ietec_logo.png">
-
     <title>Sobre</title>
 </head>
-<body>    
+
+<body>
+    <header>
+        <div class="QS_header">
+            <div class="menu_QS">
+                <a href="{{route('home')}}">Inicio</a>
+                <a href="{{route('login')}}">Login</a>
+            </div>
+            <!--menu-->
+        </div>
+        <!--header-->
+    </header>
 
     <div class="box_somos">
         <h2>O que fazemos</h2>
-        <p>Uma iniciativa destinada a abrigar e fortalecer projetos inovadores e novas empresas de base tecnológica , por um período de tempo limitado.
-            A IETEC-CEFET/RJ - Incubadora de Empresas Tecnológicas do CEFET/RJ - possui um ambiente físico que se destina a abrigar negócios e projetos que necessitem de um desenvolvimento tecnológico, de formação empreendedora e de estruturação gerencial. Constitui, assim, um meio inovador que gera condições para aumentar as chances de sobrevivência, crescimento e consolidação de microempresas inovadoras. Idealizada há mais de 20 anos, vem consolidando sua estratégia de expansão aos campi de Nova Iguaçu, Maria da Graça, Petrópolis, Valença, Angra dos Reis, Itaguaí e Nova Friburgo. Entregou ao mercado 24 empreendimentos cujo faturamento médio de 6,5 milhões e mais de 600 postos de trabalho especializados criados demonstra a força e importância das incubadoras para o ecossistema de inovação. Além disso, a IETEC CEFET/RJ assumiu a tarefa de provocar na comunidade acadêmica as discursões sobre o ensino do empreendedorismo em parceria com o SEBRAE RJ através dos simpósios anuais de Educação Empreendedora. Outra ação em andamento e em conformidade com a missão da incubadora e o pensamento das Cidades Inteligentes, Humanas e Sustentáveis apregoado pela Anprotec é a estruturação do L.U.E. - Laboratório de Uso Específico - que atenderá a comunidade de especialistas e inventores no desenvolvimento de protótipos robustos para demonstração às empresas e governos que demonstrem interesse por soluções criativas e inovadoras.
+        <p> Uma iniciativa destinada a abrigar e fortalecer projetos inovadores e novas empresas de base tecnológica, por um período de tempo limitado.
+            A IETEC-CEFET/RJ - Incubadora de Empresas Tecnológicas do CEFET/RJ - possui um ambiente físico que se destina a abrigar negócios e projetos que necessitem de um desenvolvimento tecnológico, de formação empreendedora e de estruturação gerencial.
+            Constitui, assim, um meio inovador que gera condições para aumentar as chances de sobrevivência, crescimento e consolidação de microempresas inovadoras.
+            Idealizada há mais de 20 anos, vem consolidando sua estratégia de expansão aos campi de Nova Iguaçu, Maria da Graça, Petrópolis, Valença, Angra dos Reis, Itaguaí e Nova Friburgo.
+            Entregou ao mercado 24 empreendimentos cujo faturamento médio de 6,5 milhões e mais de 600 postos de trabalho especializados criados demonstra a força e importância das incubadoras para o ecossistema de inovação.
+            Além disso, a IETEC CEFET/RJ assumiu a tarefa de provocar na comunidade acadêmica as discursões sobre o ensino do empreendedorismo em parceria com o SEBRAE RJ através dos simpósios anuais de Educação Empreendedora.
+            Outra ação em andamento e em conformidade com a missão da incubadora e o pensamento das Cidades Inteligentes, Humanas e Sustentáveis apregoado pela Anprotec é a estruturação do L.U.E. - Laboratório de Uso Específico - que atenderá a comunidade de especialistas e inventores no desenvolvimento de protótipos robustos para demonstração às empresas e governos que demonstrem interesse por soluções criativas e inovadoras.
             Assim, a IETEC perssegue o interesse público que busca soluções inovadoras para os seus processos, produtos e serviços oferecendo um espaço de diálogo e de trocas de experiências.</p>
     </div>
 
@@ -26,47 +42,43 @@
         <div class="container">
             <div class="row">
                 <div class="footer-col">
-                    <h4>-----</h4>
+                    <h4>Conteúdos</h4>
                     <ul>
                         <li>
                             <a href="#" target="blank">Apresentação</a>
+                        </li>
+                        <li>
                             <a href="#" target="blank">Direção</a>
+                        </li>
+                        <li>
                             <a href="#" target="blank">Conselhos</a>
+                        </li>
+                        <li>
                             <a href="#" target="blank">Ouvidoria</a>
                         </li>
                     </ul>
-                 </div>
-                 <div class="footer-col">
-                     <h4>Sobre Nós</h4>
-                     <ul>
-                         <li>
-                             <a href="#" target="blank">-----</a>
-                             <a href="#" target="blank">-----</a>
-                             <a href="#" target="blank">-----</a>
-                         </li>
-                     </ul>
-                 </div>
-                 <div class="footer-col"> 
-                     <h4>Contato</h4>
-                     <ul>
-                         <li>
-                             <a href="#" target="blank">Instagram</a>
-                             <a href="#">Gmail</a>
-                         </li>
-                     </ul>
-                 </div>
-                 <div class="footer-col">
-                     <h4>Páginas</h4>
-                     <ul>
-                         <li>
-                             <a href="#">Página Inicial</a>
-                             <a href="#">Jogo</a>
-                         </li>
-                     </ul>
-                 </div>
-             </div>
-         </div>   
-    </footer>    
-
+                </div>
+                <div class="footer-col">
+                    <h4>Contato</h4>
+                    <ul>
+                        <li>
+                            <a href="#" target="blank">Instagram</a>
+                            <a href="#" target="blank">Gmail</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Páginas</h4>
+                    <ul>
+                        <li>
+                            <a href="#" target="blank">Página Inicial</a>
+                            <a href="#" target="blank">Jogo</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
+
 </html>
